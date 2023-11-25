dbe1a8f00b2ac470e72eef21bf842ab8a6d6c070
dahtoos <dahtoosreal@gmail.com>
2023-11-25 14:46:44 +0100

    creating the task page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './Pages/Login_Signup/Login'
 import Signup from './Pages/Login_Signup/Signup'
 import Terms from './Pages/Login_Signup/Terms'
-
+import Tasks from './pages/login_signup/Tasks'
 
 
 function App() {
@@ -15,6 +15,7 @@ function App() {
       <Route path='/login' element={<Login/>}/>
       <Route path='/register' element={<Signup/>}/>
       <Route path='/terms' element={<Terms/>}/>
+      <Route path='/tasks' element={<Tasks/>}/>
      </Routes>
      </BrowserRouter>
     </>
---
+++ src/pages/login_signup/Tasks.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './tasks.css'
+
+const Tasks = () => {
+  return (
+    <div className='cont'>
+        <div className='hed'>
+            <h3>Task Manager App</h3>
+            <div id='name'>
+            <p id='nam'>Olatunji Damilare</p>
+            <button className='Btnnn'>Logout</button>
+            </div>
+        </div>
+     </div>
+  )
+}
+
+export default Tasks
